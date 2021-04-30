add771d4eecd580d9a91d2b85cbb833583e0f54f
Pedro Carvalho <58491291+pserr10@users.noreply.github.com>
2021-05-01 00:46:14 +0100

    Cifra numerical alphabet a dar undefined.
    Cifras ainda por conectar com frontend

---
+++ PWA/UAL/labProject/ciphers/ceasarCipher.js
@@ -0,0 +1,76 @@
+class CesarCipher {
+    constructor() { };
+
+    encrypt(str, key) {
+
+        let num = key % 26;
+
+
+        //introduzir nº positivo
+        if (num < 0) {
+            console.log("Introduza um nº maior ou igual a 0")
+        }
+
+        let output = "";
+
+        //percorre cada caracter e vai buscar o caracter que vamos juntar
+        for (let i = 0; i < str.length; i++) {
+            let charEncript = str[i];
+
+            //encontrar ascii
+            if (charEncript.match(/[a-z]/i)) {
+                //ascii code
+                let code = str.charCodeAt(i);
+
+
+                //maiusculas
+                if (code >= 65 && code <= 90) {
+                    charEncript = String.fromCharCode(((code - 65 + num) % 26) + 65);
+                }
+
+                //minusculas
+
+                else if (code >= 97 && code <= 122) {
+                    charEncript = String.fromCharCode(((code - 97 + num) % 26) + 65)
+                }
+            }
+            output += charEncript;
+        }
+        return output;
+    }
+
+    decrypt(str, key) {
+        let num = key % 26; //assegura que vamos buscar a posição correta se o nº passado for maior que 26
+        let output = "";
+
+        //percorrer a string e trazer indice
+        for (let i = 0; i < str.length; i++) {
+            let charDecript = str[i];
+
+
+            //Obter resultado concatenado, preocupar com min e maisculas dado os Ascii codes
+            //fazer o match do range e passar retirar código ASCII - .charCodeAt(i), retirar -65
+
+            if (charDecript.match(/[a-z]/i)) {
+                let code = str.charCodeAt(i);
+            
+            //fazer as maiscu .fromChartCode retirar -65
+
+            if (code >= 65 && code <= 90) {
+                charDecript = String.fromCharCode(((code + 65 - num) % 26) + 65);
+            }
+            else if (code >= 97 && code <= 122) {
+                charDecript = String.fromCharCode(((code + 96 - num) % 26) + 65);
+            }
+        }
+            output += charDecript;
+        }
+
+        return output;
+    }
+}
+
+const ceaser = new CesarCipher();
+console.log("teste"); 
+console.log(ceaser.encrypt("Nuno está rouco", 3));
+console.log(ceaser.decrypt("QXQR HVWá URXFR", 3));
---
+++ PWA/UAL/labProject/ciphers/numericalAlphabet.js
@@ -0,0 +1,44 @@
+class NumericalAlphabetCipher {
+    constructor() { };
+
+    encrypt(str, key) {
+        let num = key
+
+        if (num < 0) {
+            console.log("Introduza um nº maior ou igual a 0");
+        }
+
+        let output = "";
+
+        //itera a string
+        for (let i = 0; i < str.lenght; i++) {
+            let charEncript = str[i];
+            console.log("cenas1");
+            //.match, transforma a correspondência entre uma str e expressão regular
+            //Exp. Regular -> padrões usados p/ selecionar combinações de chars numa string
+            if (charEncript.match(/[a-z]/i)) {
+                //.charCodeAt(), encontra index ascii code
+                let code = str.charCodeAt(i);
+                console.log("cenas");
+
+                //minusculas
+                if (code >= 65 && code <= 90) {
+                    charEncript = parseInt(code);
+                }
+                else if (code >= 97 && code <= 122) {
+                    charEncript = parseInt(code);               
+                }
+                
+                output+= charEncript;
+            }
+            return output
+        }
+    }
+}
+
+
+
+
+
+const numericalAlhapet = new NumericalAlphabetCipher;
+console.log(numericalAlhapet.encrypt("alerta", 12));
