2691e09fe50adddf9cd06de389c96e5ad4a23c7b
pserr10 <goncalo.crvlho@gmail.com>
2021-05-04 01:28:22 +0100

    sw main.js

--- src/main.js
+++ src/main.js
@@ -56,3 +56,13 @@ class Main {
 
 const main = new Main();
 firebase.initializeApp(firebaseConfig);
+if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  }
+  
