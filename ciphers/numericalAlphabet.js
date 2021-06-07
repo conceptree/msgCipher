5504db464c19af3cdcb7dd9ed7bd87911382b869
Nuno Rodrigues <nunorodrigues@conceptree.com>
2021-06-07 22:12:21 +0100

    numerical description

--- ciphers/numericalAlphabet.js
+++ ciphers/numericalAlphabet.js
@@ -1,45 +1,38 @@
 export class NumericalAlphabetCipher {
     constructor() {
         this.alphabet = ["a", "b", "c", "d", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-        this.nums = []
-        this.splitToDigit = function(numbers){
-            return (numbers + '').split('').map((i) => {
-                return Number(i);
-            })
-        }
-    };
-
-  
+        this.nums = [];
+        this.createNumsArray = (key) => {
+            for (let i = key; i < key + 25; i++) {
+                this.nums.push(i);
+            }
+        };
+    }
 
     encrypt(str, key) {
         let result = [];
 
+       this.createNumsArray(Number(key));
 
-        for (let i = key; i < key + 25; i++) {
-            this.nums.push(i);
-        }
-
-        
         str.split("").forEach(char => {
-            result.push(this.nums[this.alphabet.indexOf(char.toLowerCase())]);
+            result.push(this.nums[this.alphabet.indexOf(char.toLowerCase())], ",");
         });
 
         return result.join("");
 
     }
 
-    decryp(numbers,key){
+    decrypt(str,key){
         let result = [];
 
-        for(let i = key; i < key + 25; i++){
-            this.nums.push(i);
-        }
+        this.createNumsArray(Number(key));
 
-        this.splitToDigit(numbers).forEach(numbers => {
-            result.push(this.nums[this.alphabet.valueOf(numbers)]);
+        str.split(",").forEach(num => {
+            result.push(this.alphabet[this.nums.indexOf(Number(num))]);
         });
 
         return result.join("");
     }
+
 }
 
