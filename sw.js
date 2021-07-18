42e670d26d8efad33d1ff271741ca445b1740b3d
Nuno Rodrigues <nunomtrodrigues@gmail.com>
2021-07-18 20:06:22 +0100

    sw changes

--- sw.js
+++ sw.js
@@ -1,51 +1,23 @@
-var APP_PREFIX = 'MsgCipher';
-var VERSION = 'version_05';
-var CACHE_NAME = APP_PREFIX + VERSION;
-var URLS = [
-  'https://conceptree.github.io/msgCipher/',
-  '/',
-  '/index.html'
-];
-
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) {
-        console.log('responding with cache : ' + e.request.url);
-        return request;
-      } else {
-        console.log('file is not cached, fetching : ' + e.request.url);
-        return fetch(e.request);
-      }
-
-    }));
-});
-
-self.addEventListener('install', function (e) {
+self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME);
-      return cache.addAll(URLS);
-    })
-  );
-});
-
-self.addEventListener('activate', function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-
-      var cacheWhitelist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX);
-      });
-
-      cacheWhitelist.push(CACHE_NAME);
-
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i]);
-          return caches.delete(keyList[i]);
-        }
-      }));
-    }));
-});
+     caches.open('MsgCipher_06').then(cache => {
+         return cache.addAll([
+             '/',
+             '/index.html'
+         ])
+             .then(() => self.skipWaiting());
+     })
+   )
+ });
+ 
+ self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+ });
+ 
+ self.addEventListener('fetch', event => {
+   event.respondWith(
+     caches.match(event.request).then(response => {
+         return response || fetch(event.request);
+     })
+   );
+ });
